e69b3910fcc175bdb0de1e951df64e3f744ca5e3
Jean-marc WANCTIN <jewancti@e2r3p20.clusters.42paris.fr>
2023-01-24 23:13:59 +0100

    fix $

--- parse/parse_input.c
+++ parse/parse_input.c
@@ -6,7 +6,7 @@
 /*   By: jewancti <jewancti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 12:42:43 by jewancti          #+#    #+#             */
-/*   Updated: 2023/01/24 21:29:51 by jewancti         ###   ########.fr       */
+/*   Updated: 2023/01/24 23:11:43 by jewancti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,10 @@ void	attribute_args(int *start, int *index_args, char **parse, t_cmd *ptr)
 					&& str[1] == '\0')
 				|| (ft_strcmp(str, ">>") == 0 || ft_strcmp(str, "<<") == 0))
 				break ;
+			while (parse[(*start)] && parse[(*start)][0] == VARVIDE)
+				(*start)++;
+			if (!parse[*start] || parse[*start][0] == '|')
+				return ;
 			ptr -> args[(*index_args)++] = ft_strdup(parse[(*start)++]);
 		}
 	}
@@ -73,7 +77,7 @@ void	set_sequence(t_cmd **ptr, char **arrays[2], int *index[3])
 
 	parse = arrays[0];
 	split = arrays[1];
-	if (parse[*index[0]])
+	if (parse[*index[0]] && parse[*index[0]][0] != VARVIDE)
 	{
 		(*ptr)->length_sequence = count_occurence(split[*index[2]], '>')
 			+ count_occurence(split[*index[2]], '<');
@@ -87,8 +91,12 @@ void	set_sequence(t_cmd **ptr, char **arrays[2], int *index[3])
 		(*index[0])++;
 	if (!(*ptr)->command && (*ptr)->args && (*ptr)->args[0])
 		(*ptr)->command = ft_strdup((*ptr)->args[0]);
-	if (parse[*index[0]] && parse[*index[0]][0] != VARVIDE)
+	if (parse[*index[0]])
 	{
+		//if (parse[*index[0]][0] == VARVIDE && (*ptr) -> command == NULL)
+		//	return ;
+		//while (parse[*index[0]] && parse[*index[0]][0] == VARVIDE)
+		//	(*index[0])++;
 		(*ptr)->next = ft_calloc(sizeof(t_cmd), 1);
 		(*ptr) = (*ptr)->next;
 	}
@@ -113,6 +121,8 @@ int	loop(t_data *data, t_cmd **cmd, char **parse, char **split)
 			index++;
 		if (!parse[index])
 			break ;
+		if (parse[index][0] == '|' || parse[index][0] == VARVIDE)
+			index++;
 		while (parse[index][i] == '>' || parse[index][i] == '<')
 			i++;
 		if (parse[index][i] != '\0')
